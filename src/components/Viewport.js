a3aaa6791edf537b4f7c124a1fc1b2d50945883c
Charles Taylor <charlest@paragontruss.com>
2019-08-08 20:28:01 -0400

    Resizes cleanly

--- src/components/Viewport.js
+++ src/components/Viewport.js
@@ -1,19 +1,15 @@
 import React, { useState, useCallback, useRef } from 'react'
 import './Viewport.css'
+import useResizeAware from 'react-resize-aware'
 
 export const Viewport = () => {
+  const [ resizeListener, size ] = useResizeAware()
   const [ nodes, setNodes ] = useState([])
-  const ref = useRef(null)
-
-  const getViewportDimensions = () => {
-    if (!ref.current) return undefined
-    return ref.current.getBoundingClientRect()
-  }
 
   const onClick = useCallback(
     e => {
       const { nativeEvent: { offsetX, offsetY } } = e;
-      const { width, height } = getViewportDimensions();
+      const { width, height } = size;
       const point = {
         x: Math.min(offsetX / width, 1),
         y: Math.min(offsetY / height, 1),
@@ -25,27 +21,28 @@ export const Viewport = () => {
         ]
       )
     },
-    [setNodes]
+    [setNodes, size]
   )
-
-  const box = getViewportDimensions();
   return (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      width="100%"
-      height="100%"
+    <div
       className="viewport"
-      onClick={onClick}
     >
-      {nodes.map((node, i) =>
-        <circle
-          key={i}
-          r="4"
-          cx={node.x * box.width}
-          cy={node.y * box.height}
-        />)
-      }
-    </svg>
+      {resizeListener}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        onClick={onClick}
+      >
+        {nodes.map((node, i) =>
+          <circle
+            key={i}
+            r="4"
+            cx={node.x * size.width}
+            cy={node.y * size.height}
+          />)
+        }
+      </svg>
+    </div>
   )
 }
