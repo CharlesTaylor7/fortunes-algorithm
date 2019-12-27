fdda8f35c6fb6cd99c80597b03daf0f92bae23f9
Charles Taylor <charlestaylor95@gmail.com>
2019-12-27 17:24:31 -0300

    Fix parabola stroke

--- src/components/Parabola.js
+++ src/components/Parabola.js
@@ -3,6 +3,9 @@ import { parabolaPath } from '../utilities/parabola'
 
 export const Parabola = ({ focus, viewportHeight }) => (
   <path
+    stroke="black"
+    strokeWidth="2"
+    fill="none"
     d={parabolaPath({
       focus,
       directrix: 0,
