7b187705e69bb2aca5b8a830d0a59c7bf3ca6cf3
Charles Taylor <charlestaylor95@gmail.com>
2019-12-28 10:29:49 -0300

    Draw just focus & parabola

--- src/components/svg/Parabola.js
+++ src/components/svg/Parabola.js
@@ -5,26 +5,8 @@ import { Path } from './Path'
 export const Parabola = ({ start, control, end, focus, directrix }) => (
   <>
     <Path
-      color="blue"
-      path={`M ${control.x} ${control.y} H ${focus.x}`}
-    />
-    <Path
       color="black"
       path={`M ${start.x} ${start.y} Q ${control.x} ${control.y} ${end.x} ${end.y}`}
-    />
-    <Path
-      color="red"
-      path={`M ${start.x} ${start.y} L ${control.x} ${control.y} L ${end.x} ${end.y}`}
-    />
-    <Point
-      color="blue"
-      x={control.x}
-      y={control.y}
-    />
-    <Point
-      color="green"
-      x={(directrix + focus.x) / 2}
-      y={focus.y}
     />
     <Point
       color="black"
