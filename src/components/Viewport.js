b9894f2a829af1006aadafe3ae107bfae2191128
Charles Taylor <charlestaylor95@gmail.com>
2020-01-05 20:13:56 -0300

    Remove redundant namespace attribute

--- src/components/Viewport.js
+++ src/components/Viewport.js
@@ -26,7 +26,6 @@ export const Viewport = () => {
       {viewportSizeListener}
       <svg
         className="svg"
-        xmlns="http://www.w3.org/2000/svg"
         width="100%"
         height="100%"
       >
