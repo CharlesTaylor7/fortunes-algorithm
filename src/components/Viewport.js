d0e95056ce7882b9d162b761593da7af27edc108
Charles Taylor <charlestaylor95@gmail.com>
2020-01-05 19:44:01 -0300

    Extract viewport logic to hook

--- src/components/Viewport.js
+++ src/components/Viewport.js
@@ -1,62 +1,22 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React from 'react'
 import './Viewport.css'
-import useResizeAware from 'react-resize-aware'
 import { Parabola } from './svg/Parabola'
 import { Site } from './svg/Site'
 import { parabolaBezier } from '../utilities/parabola'
-import getOffsetFromCurrentTarget from '../utilities/getOffsetFromCurrentTarget'
 import { Sweepline } from './svg/Sweepline'
-import useAnimation from '../hooks/useAnimation'
+import useViewport from '../hooks/useViewport'
 
 export const Viewport = () => {
-  const [ viewportSizeListener, viewportSize ] = useResizeAware();
-  const [ sites, setSites ] = useState([]);
-  const onClick = useCallback(
-    event => {
-      const site = getOffsetFromCurrentTarget(event);
-      setSites(ns => [...ns, site]);
-    },
-    []
-  );
-
-  const [sweeplineX, setSweeplineX, animateSweepline] = useAnimation(200);
-  const [sweeplineDragging, setSweeplineDragging] = useState(false);
-  const onClickSweepline = useCallback(
-    event => {
-      event.stopPropagation();
-      setSweeplineDragging(!sweeplineDragging);
-    },
-    [sweeplineDragging, setSweeplineDragging]
-  );
-
-  const onMouseMove = useCallback(
-    event => {
-      const offset = getOffsetFromCurrentTarget(event);
-      if (sweeplineDragging) {
-        setSweeplineX(offset.x);
-      }
-    },
-    [sweeplineDragging, setSweeplineX]
-  );
-
-  const [index, setIndex] = useState(-1);
-  useEffect(() => {
-    document.onkeydown = (e) => {
-      if (e.key === 'ArrowLeft') {
-        if (sites[index - 1]) {
-          setIndex(index - 1);
-          animateSweepline(sites[index - 1].x)
-        }
-      }
-      else if (e.key === 'ArrowRight') {
-        if (sites[index + 1]) {
-          setIndex(index + 1);
-          animateSweepline(sites[index + 1].x)
-        }
-      }
-    }
-  }, [index, setIndex, sites])
-
+  const {
+    sweeplineX,
+    onClick,
+    onClickSweepline,
+    onMouseMove,
+    viewportSize,
+    viewportSizeListener,
+    sweeplineDragging,
+    sites,
+  } = useViewport();
   return (
     <div
       className="viewport"
---
+++ src/hooks/useViewport.js
@@ -0,0 +1,65 @@
+import { useState, useCallback, useEffect } from 'react'
+import getOffsetFromCurrentTarget from '../utilities/getOffsetFromCurrentTarget'
+import useAnimation from '../hooks/useAnimation'
+import useResizeAware from 'react-resize-aware'
+
+export default () => {
+
+  const [ viewportSizeListener, viewportSize ] = useResizeAware();
+  const [ sites, setSites ] = useState([]);
+  const onClick = useCallback(
+    event => {
+      const site = getOffsetFromCurrentTarget(event);
+      setSites(ns => [...ns, site]);
+    },
+    []
+  );
+
+  const [sweeplineX, setSweeplineX, animateSweepline] = useAnimation(200);
+  const [sweeplineDragging, setSweeplineDragging] = useState(false);
+  const onClickSweepline = useCallback(
+    event => {
+      event.stopPropagation();
+      setSweeplineDragging(!sweeplineDragging);
+    },
+    [sweeplineDragging, setSweeplineDragging]
+  );
+
+  const onMouseMove = useCallback(
+    event => {
+      const offset = getOffsetFromCurrentTarget(event);
+      if (sweeplineDragging) {
+        setSweeplineX(offset.x);
+      }
+    },
+    [sweeplineDragging, setSweeplineX]
+  );
+
+  const [index, setIndex] = useState(-1);
+  useEffect(() => {
+    document.onkeydown = (e) => {
+      if (e.key === 'ArrowLeft') {
+        if (sites[index - 1]) {
+          setIndex(index - 1);
+          animateSweepline(sites[index - 1].x)
+        }
+      }
+      else if (e.key === 'ArrowRight') {
+        if (sites[index + 1]) {
+          setIndex(index + 1);
+          animateSweepline(sites[index + 1].x)
+        }
+      }
+    }
+  }, [index, setIndex, sites])
+
+  return {
+    sweeplineX,
+    viewportSize,
+    viewportSizeListener,
+    onClick,
+    onMouseMove,
+    onClickSweepline,
+    sites,
+  };
+}
