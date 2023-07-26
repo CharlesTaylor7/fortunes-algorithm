a97c9b69b369e2fd75b5c35c9988b51c5a8aa616
Charles Taylor <charlestaylor95@gmail.com>
2023-07-26 14:57:55 -0400

    Rename project to fortune's algorithm

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { name } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/voronoi/',
+  base: `/${name}/`,
   esbuild: {
     keepNames: true,
   },
