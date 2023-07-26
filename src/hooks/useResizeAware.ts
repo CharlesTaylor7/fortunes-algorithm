60e6e56af342e79d271fdacf97576f1f9ba45ac2
Charles Taylor <charlestaylor95@gmail.com>
2023-07-26 12:21:24 -0400

    Resize doesn't work

--- src/hooks/useResizeAware.ts
+++ src/hooks/useResizeAware.ts
@@ -1,6 +1,7 @@
 import { Ref, useState, useCallback, useEffect, useRef } from 'react'
 import type { BoundingBox } from '@/utilities/types'
 
+
 export default function useResizeAware(): [Ref<HTMLElement>, BoundingBox] {
   const ref = useRef<HTMLElement>(null)
   const [bounds, setBounds] = useState<BoundingBox>({height: 0, width: 0})
@@ -11,10 +12,16 @@ export default function useResizeAware(): [Ref<HTMLElement>, BoundingBox] {
       console.error("useResizeAware ref is not bound to any mounted react component")
       return
     }
-    setBounds(targetEl.getBoundingClientRect())
+    setBounds(rectToBounds(targetEl))
     targetEl.addEventListener('resize', () => {
-      setBounds(targetEl.getBoundingClientRect())
+      setBounds(rectToBounds(targetEl))
     })
   }, [setBounds, ref.current])
   return [ref, bounds]
+}
+
+function rectToBounds(target: HTMLElement): BoundingBox {
+  const domRect = target.getBoundingClientRect()
+  console.log(domRect)
+  return { height: domRect.height, width: domRect.width }
 }
